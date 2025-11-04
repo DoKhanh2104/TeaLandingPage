e3f28ff0f0c07a00989494875c9384ce56988320
DoHungQuocKhanh <>
2025-11-04 23:47:46 +0700

    update header and products layout

--- js/main.js
+++ js/main.js
@@ -12,8 +12,20 @@ $(function () {
 })
 
 //Tab
+
+//Change color
 $(function () {
-    $('#products-tabs').responsiveTabs({
-        startCollapsed: 'accordion'
+    //Add activeTab into first li
+    $("li:first").addClass("activeTab");
+
+    //Chang color when click
+    $("li").on("click", function () {
+        $("li").removeClass("activeTab")
+        $('div[id="products-tabs"] ul .r-tabs-state-active').addClass("activeTab")
     });
-})
+
+    $("#products-tabs").responsiveTabs({
+        animation: "slide",
+    });
+}
+);
